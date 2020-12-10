c8218acd6fe9a431368530f2ae9c1f992a7120d3
OscarGB <oscar.gomez.borz@gmail.com>
2020-12-10 21:00:54 +0100

    falta la memoria (creo)

--- Practica3/Ejercicio1.R
+++ Practica3/Ejercicio1.R
@@ -1,3 +1,7 @@
+library(gplots)
+library(RColorBrewer)
+library(qgraph)
+
 # Cargar Datos
 data <- read.csv("http://cardsorting.net/tutorials/25.csv")
 
@@ -7,8 +11,46 @@ data$Uniqid <- NULL
 data[2:5] <- NULL
 
 # Pasar categoría a minúscula (hay iguales)
-data$Category <- tolower(data$Category)
+# data$Category <- tolower(data$Category)
 
 # Agregar las filas con la misma categoría sumando apariciones
-data <- aggregate(. ~ Category, data, sum)
+# data <- aggregate(. ~ Category, data, sum)
 
+# Histograma de todos los valores, vemos que son todo ceros y unos,
+# Además vemos que aparecen muchos más ceros que unos (9:1)
+hist(data.matrix(data[,-1]), main="Histograma de los valores de los datos")
+
+# Trasponemos el dataframe
+data <- as.data.frame(t(as.matrix(data)))
+# Para poderlo usar con la función dist (que calcula la distancia entre 
+# filas), quitamos la primera fila ya que es el nombre de la categoría
+dst <- dist(data[-1,], diag=TRUE)
+
+# Lo ponemos como matriz de datos numérica
+dst <- data.matrix(dst)
+
+# Obtenemos el número de categorías
+dim <- ncol(dst)
+
+# Dibujar una imagen donde el eje "z" es la distancia, de esta manera
+# el color toma valores oscuros para distancias bajas
+image(1:dim, 1:dim, -dst, axes = FALSE, xlab="", ylab="")
+
+# Colocamos los nombres de las categorías en los ejes
+axis(1, 1:dim, row.names(data)[-1], cex.axis = 0.5, las=3)
+axis(2, 1:dim, row.names(data)[-1], cex.axis = 0.5, las=1)
+
+# Colocamos los valores de las distancias en las posiciones correpsondientes
+text(expand.grid(1:dim, 1:dim), sprintf("%0.1f", dst), cex=0.6)
+
+# Hacemos el grafo de distancias. Hacemos 1/dst porque cuanto menor distancia
+# mayor tamaño de arista.
+dst <- 1/dst
+qgraph(dst, layout='spring', labels = colnames(dst))
+
+# Parece que las tarjetas más relacionadas son:
+# Cake <-> Pie (Tartas)
+# Pancakes <-> Waffles (Dulces de desayuno)
+# Steak <-> Chicken (Carnes)
+# Carrots <-> Onions <-> Brocoli <-> Lettuce (Verduras)
+# Apple <-> Orange <-> Pineapple <-> Banana <-> Watermelon (Frutas)
